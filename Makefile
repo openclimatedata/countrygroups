e77f4feb07b3c4885368a03c01773b107f40bc0d
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-07-10 12:56:50 +0200

    Add AOSIS

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CSV_FILES = data/unfccc.csv data/annex-one.csv data/non-annex-one.csv data/ldcs.csv data/graduated-ldcs.csv data/eu-member-states.csv data/sids.csv data/sids-non-un-or-regional-commissions-associates.csv data/g20.csv data/g7.csv data/oecd.csv data/brics.csv data/umbrella.csv data/opec.csv
+CSV_FILES = data/unfccc.csv data/annex-one.csv data/non-annex-one.csv data/ldcs.csv data/graduated-ldcs.csv data/eu-member-states.csv data/sids.csv data/sids-non-un-or-regional-commissions-associates.csv data/g20.csv data/g7.csv data/oecd.csv data/brics.csv data/umbrella.csv data/opec.csv data/aosis.csv
 
 all: $(CSV_FILES) unfccc_groups/__init__.py index.js
 
@@ -47,6 +47,10 @@ data/umbrella.csv: scripts/umbrella.py
 	@./venv/bin/python $<
 
 data/opec.csv: scripts/opec.py
+	@echo $@
+	@./venv/bin/python $<
+
+data/aosis.csv: scripts/aosis.py
 	@echo $@
 	@./venv/bin/python $<
 
---
+++ scripts/aosis.py
@@ -0,0 +1,67 @@
+# AOSIS
+
+import os
+import pandas as pd
+from util import to_code
+
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+countries = [
+    "Antigua and Barbuda",
+    "Bahamas",
+    "Barbados",
+    "Belize",
+    "Cape Verde",
+    "Comoros",
+    "Cook Islands",
+    "Cuba",
+    "Dominica",
+    "Dominican Republic",
+    "Fiji",
+    "Federated States of Micronesia",
+    "Grenada",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Jamaica",
+    "Kiribati",
+    "Maldives",
+    "Marshall Islands",
+    "Mauritius",
+    "Nauru",
+    "Niue",
+    "Palau",
+    "Papua New Guinea",
+    "Samoa",
+    "Singapore",
+    "Seychelles",
+    "Sao Tome and Principe",
+    "Solomon Islands",
+    "St. Kitts and Nevis",
+    "St. Lucia",
+    "St. Vincent and the Grenadines",
+    "Suriname",
+    "Timor-Leste",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tuvalu",
+    "Vanuatu"
+]
+
+# Observers:
+# American Samoa
+# Netherlands Antilles
+# Guam
+# U.S. Virgin Islands
+# Puerto Rico
+
+
+index = [to_code(country) for country in countries]
+aosis = pd. DataFrame({"Name": countries}, index=index)
+aosis.index.name = "Code"
+
+aosis.to_csv(os.path.join(path, "../data/aosis.csv"))
+
+assert len(aosis) == 44 - 5  # 44 States and observers
