93bd1d77302806efd87fa459a328b6831c62c590
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-07-11 14:05:05 +0200

    Add short country names for convenience

--- scripts/generate_modules.py
+++ scripts/generate_modules.py
@@ -5,6 +5,7 @@ listed in Data Package.
 
 import os
 import pandas as pd
+import pprint
 
 from pandas_datapackage_reader import read_datapackage
 
@@ -13,8 +14,10 @@ path = os.path.dirname(os.path.realpath(__file__))
 
 dp = read_datapackage(os.path.join(path, "../datapackage.json"))
 
+codes = []
+
 py_out = '''"""
-unfccc_groups
+countrygroups
 -------------
 
 """
@@ -26,15 +29,18 @@ del get_versions
 
 '''
 
-js_out = '''
-// UNFCCC Groups
+js_out = '''// Country Groups
+
 '''
 
 for name, df in sorted(dp.items()):
-    if isinstance(df.index, pd.RangeIndex):
+    if (name == "shortnames") or isinstance(df.index, pd.RangeIndex):
         continue
     group_id = name.replace("-", "_").upper()
     members = sorted(df.index.tolist())
+    for member in members:
+        if member not in codes:
+            codes.append(member)
     py_out += "{} = [\n".format(group_id)
     js_out += "exports.{} = [\n".format(group_id)
     for idx, item in enumerate(members):
@@ -49,6 +55,16 @@ for name, df in sorted(dp.items()):
             js_out += "\n"
     py_out += "]\n\n"
     js_out += "]\n\n"
+
+assert len(codes) == len(dp["shortnames"])
+shortnames = pprint.pformat(
+    dp["shortnames"].to_dict(orient="dict")["Name"], indent=4)[1:-1]
+py_out += "shortnames = {\n "
+py_out += shortnames
+py_out += "\n}\n"
+js_out += "export.shortnames = {\n "
+js_out += shortnames
+js_out += "\n}\n"
 
 with open(os.path.join(path, "../countrygroups/__init__.py"), "w") as f:
     f.write(py_out)
