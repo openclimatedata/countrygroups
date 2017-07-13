5fc19a17d517d6ca7e84fdce63958dc72ae82a70
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-07-13 11:44:37 +0200

    Use pathlib

--- scripts/arab-group.py
+++ scripts/arab-group.py
@@ -2,12 +2,9 @@
 
 # http://unfccc.int/parties_and_observers/parties/negotiating_groups/items/2714.php
 
-import os
 import pandas as pd
-from util import to_code
+from util import to_code, root
 
-
-path = os.path.dirname(os.path.realpath(__file__))
 
 countries = [
     "Algeria",
@@ -38,6 +35,6 @@ index = [to_code(country) for country in countries]
 arabgroup = pd. DataFrame({"Name": countries}, index=index)
 arabgroup.index.name = "Code"
 
-arabgroup.to_csv(os.path.join(path, "../data/arab-group.csv"))
+arabgroup.to_csv(root / "data/arab-group.csv")
 
 assert len(arabgroup) == 22
