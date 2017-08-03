0d65944bff60d8ad1896cf80d164120b13752e72
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-08-03 21:49:24 +0200

    Add submodule creation

--- scripts/generate_modules.py
+++ scripts/generate_modules.py
@@ -3,17 +3,17 @@ Generate Python and JavaScript modules with ISO identifier codes of groups
 listed in Data Package.
 """
 
-import os
 import pandas as pd
 import pprint
 
+from  pathlib import Path
 from pandas_datapackage_reader import read_datapackage
 
 
-path = os.path.dirname(os.path.realpath(__file__))
+root = Path(__file__).parents[1]
 
-dp = read_datapackage(os.path.join(path, "../datapackage.json"))
-sn = read_datapackage("https://github.com/rgieseke/shortcountrynames")
+dp = read_datapackage(root / "datapackage.json")
+
 codes = []
 
 py_out = '''"""
@@ -33,7 +33,33 @@ js_out = '''// Country Groups
 
 '''
 
+py_submodules = []
+
+def create_submodule(name, df):
+    py_submodule_out = '"""\ncountrygroups.{}\n--------------'.format(
+        name) + len(name) * "-" + '\n"""'
+    py_submodule_out += "\n\n"
+    grouped = df.groupby("Region")
+    for region, group in grouped:
+        py_submodule_out += "{} = [\n".format(region)
+        members = sorted(group.index.tolist())
+        for idx, member in enumerate(members):
+            item_line = '    "{}"'.format(member)
+            py_submodule_out += item_line
+            if idx < len(members) - 1:
+                py_submodule_out += ",\n"
+            else:
+                py_submodule_out += "\n"
+        py_submodule_out += "]\n\n"
+
+    with open(root / "countrygroups" / (name + ".py"), "w") as f:
+        f.write(py_submodule_out)
+
 for name, df in sorted(dp.items()):
+    if "subregions" in df.metadata:
+        create_submodule(name, df)
+        continue
+
     if isinstance(df.index, pd.RangeIndex):
         continue
     group_id = name.replace("-", "_").upper()
@@ -57,8 +83,8 @@ for name, df in sorted(dp.items()):
     js_out += "]\n\n"
 
 
-with open(os.path.join(path, "../countrygroups/__init__.py"), "w") as f:
+with open(root / "countrygroups/__init__.py", "w") as f:
     f.write(py_out)
 
-with open(os.path.join(path, "../index.js"), "w") as f:
+with open(root / "index.js", "w") as f:
     f.write(js_out)
