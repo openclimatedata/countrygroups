635d86d67d18c57ff535ef153a2730e4743fa77a
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-08-10 22:21:34 +0200

    Tag script fixes

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ `git status --porcelain` ]]; then
+sed="sed";
+uname=$(uname);
+case "$uname" in
+    (*Darwin*) sed='gsed'; ;;
+
+esac;
+
+
+if [[ "$(git status --porcelain)" ]]; then
   printf "Working directory not clean:\n\n"
   git status
 else
@@ -8,12 +16,13 @@ else
   git tag
 
   echo Commits since last tag:
-  git log `git describe --tags --abbrev=0`..HEAD --oneline
+  git log "$(git describe --tags --abbrev=0)"..HEAD --oneline
   echo "Please enter next version (x.y.z):"
   read version
   echo "You entered: $version"
-  sed -i -r "s/(\"version\": \").*(\")/\1$version\2/" datapackage.json
+  "$sed" -i -r "s/(\"version\": \").*(\")/\1$version\2/" datapackage.json
   git add datapackage.json
   git commit -m "Update version in datapackage.json"
-  npm version $version -m "Update version to %s"
+  npm version "$version" -m "Update version to %s"
 fi
+
