51f70faa377fc46a10a09cbfb05df640d78ab15e
Robert Gieseke <rob.g@web.de>
2026-06-12 12:42:03 +0200

    Update G77 and IMO

--- scripts/imo.py
+++ scripts/imo.py
@@ -8,8 +8,10 @@ from util import root, to_code
 from shortcountrynames import to_name
 
 
-url = ("https://treaties.un.org/pages/ViewDetails.aspx?"
-       "src=TREATY&mtdsg_no=XII-1&chapter=12&clang=_en")
+url = (
+    "https://treaties.un.org/pages/ViewDetails.aspx?"
+    "src=TREATY&mtdsg_no=XII-1&chapter=12&clang=_en"
+)
 
 page = requests.get(url)
 tree = html.fromstring(page.content)
@@ -17,8 +19,10 @@ tree = html.fromstring(page.content)
 for superscript in tree.xpath("//sup"):
     superscript.getparent().remove(superscript)
 
-member_states = tree.xpath('//table[@id="'
-    'ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_tblgrid"]')
+member_states = tree.xpath(
+    '//table[@id="'
+    'ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_tblgrid"]'
+)
 table = member_states[0]
 df = pd.read_html(etree.tostring(table), skiprows=1)[0]
 
@@ -29,12 +33,14 @@ df.index = df["Name"].apply(to_code)
 df.index.name = "Code"
 df.Name = [to_name(i) for i in df.index]
 
+
 def parse_date(datestring):
     datestring = datestring.rsplit(" ", 1)[0]
     return pd.to_datetime(datestring)
 
+
 df.Date = df.Date.apply(parse_date)
 
-assert len(df) == 175
+assert len(df) == 176
 
 df.to_csv(root / "data/imo.csv")
